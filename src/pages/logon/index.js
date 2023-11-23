681e16eb2914607db61e79207c8a0dba58d992d4
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2023-11-22 21:38:35 -0300

    Add Menu

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -2,15 +2,19 @@ import './styles.css'
 import Logo from '../../assets/img/logo.jpg'
 
 
+
+
 export default function Logon(){
-   return(
+  
+
+    return(
     <div className="logon-container">
         <div className='logo'>
            <img src={Logo} />
         </div>
         <section className="form">
             <h1>Faça seu login</h1>
-            <form >
+            <form  >
                 <input placeholder="Email" />
                 <input placeholder="Senha" type='password' />
                 <button type="submit">Entrar</button>
