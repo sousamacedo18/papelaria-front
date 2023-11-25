0e07c10b6ab4d4622709997a0f5703d9eda55f8a
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2023-11-24 21:22:11 -0300

    alterado lista de usuários

--- src/pages/listarUsuario/index.js
+++ src/pages/listarUsuario/index.js
@@ -3,6 +3,13 @@ import Logo from '../../assets/img/logo.jpg'
 import Menu from '../../componentes/menu'
 
 export default function Listausuario(){
+
+    const dados=[
+        {id:1,nome:"Carlos",email:"carlos@gmail.com",senha:"123"},
+        {id:2,nome:"Felipe",email:"felipe@gmail.com",senha:"321"},
+        {id:3,nome:"Nilson",email:"nilson@gmail.com",senha:"321"},
+
+    ]
    return(
     <div className="dashboard-container">
         <div className='menu'>
@@ -11,6 +18,26 @@ export default function Listausuario(){
         </div>
         <div className='principal'>
         <h1>Lista de Usuários</h1>
+        <table className="table">
+           <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th></th>
+            </tr>
+            {
+               dados.map((usu)=>{
+                return(
+                  <tr key={usu.toString()}>
+                    <td>{usu.id}</td>    
+                    <td>{usu.nome}</td>    
+                    <td>{usu.email}</td>    
+                  </tr>  
+                )
+               }) 
+            }
+
+        </table>
         </div>
     </div>
 
