4a049671462514b0ec40d8e7bb8f261f4c52dc87
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2023-11-27 21:41:39 -0300

    adicionei icones na tabela

--- src/pages/listarUsuario/index.js
+++ src/pages/listarUsuario/index.js
@@ -1,7 +1,8 @@
 import '../../pages/global.css';
 import Logo from '../../assets/img/logo.jpg'
 import Menu from '../../componentes/menu'
-
+import{ FiEdit,FiTrash,FiDelete, FiFilePlus }from "react-icons/fi";
+import { Bs5Circle } from "react-icons/bs";
 export default function Listausuario(){
 
     const dados=[
@@ -18,20 +19,27 @@ export default function Listausuario(){
         </div>
         <div className='principal'>
         <h1>Lista de Usuários</h1>
+        <button className='btn-novo'>Novo Cadastro</button>
         <table className="table">
            <tr>
                 <th>Id</th>
                 <th>Nome</th>
                 <th>Email</th>
                 <th></th>
+                <th></th>
             </tr>
             {
-               dados.map((usu)=>{
+               dados.map((linha)=>{
                 return(
-                  <tr key={usu.toString()}>
-                    <td>{usu.id}</td>    
-                    <td>{usu.nome}</td>    
-                    <td>{usu.email}</td>    
+                  <tr key={linha.toString()}>
+                    <td>{linha.id}</td>    
+                    <td>{linha.nome}</td>    
+                    <td>{linha.email}</td>    
+                    <td className='botoes'> 
+                      <FiEdit size={18} color='#3a5795'  />  
+                    </td>    
+                    <td className='botoes'> <FiTrash size={18} color='red' /> </td>    
+                    
                   </tr>  
                 )
                }) 
