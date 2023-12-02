93b1b22395696e7a7361ace5e203cb7757ffd7cc
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2023-12-01 21:39:06 -0300

    alteração no cadastro de usuários

--- src/pages/cadastroUsuario/index.js
+++ src/pages/cadastroUsuario/index.js
@@ -1,10 +1,16 @@
+import React,{useState} from 'react';
+
 import '../../pages/global.css';
 import Menu from '../../componentes/menu'
 import{ FiFilePlus } from "react-icons/fi";
+import { MdCancel } from "react-icons/md";
+import { FaSave } from "react-icons/fa";
 import {Link} from 'react-router-dom';
 import Head from '../../componentes/Head';
 
 export default function Cadastrousuario(){
+
+  const [nome,setNome]  = useState("");
 
   return(
     <div className="dashboard-container">
@@ -15,15 +21,28 @@ export default function Cadastrousuario(){
         </div>
         <div className='principal'>
         <Head title="Cadastro de Usuário" />
-
-        <form>
-            <input type='text' placeholder='Digite o nome do usuário' />
+        <div className='form-container'>
+        <form className='form-cadastro' >
+            <input 
+            type='text'
+            value={nome}
+            onChange={e=>setNome(e.target.value)}
+             placeholder='Digite o nome do usuário'
+              />
             <input type='email' placeholder='Digite o email' />
             <input type='password' placeholder='Digite a senha' />
-            <button >Salvar</button>
-            <button >Cancelar</button>
+            <div className='acao'>
+            <button className='btn-save'>
+               <FaSave />
+              Salvar
+            </button>
+            <button className='btn-cancel'>
+            <MdCancel />
+              Cancelar</button>
+            </div>
         </form>
-
+    
+        </div>
         </div>
     </div>
 
